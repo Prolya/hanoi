1bcf8d7f081ecc35461f9457b7f495bdc17c9f76
Permyakova Olga <permyakovao.a.16@gmail.com>
2021-11-17 19:48:23 +0700

    refactor

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
+const  FACTORIAL_RODS_3 = 6;
 
-// n - количество кружков; start - начало головоломки; finish - стержень, на котором должна оказаться башня
-function hanoi(n, start, finish) { 
+function hanoi(numberCircles, startRod, finishRod) { 
 
-    // Вспомогательный стержень
-    let tmp = 6 - start - finish; 
+    let tmpRod = FACTORIAL_RODS_3 - startRod - finishRod;     
+ 
+    if (numberCircles == 1) {
 
-    if (n == 1) {
-
-        console.log("Кружок " + n + " сместиться с " + start + " на " + finish);
+        print(numberCircles, startRod, finishRod);
 
     } else {
 
-        hanoi(n - 1, start, tmp);
+        hanoi(numberCircles - 1, startRod, tmpRod);
 
-        console.log ("Кружок " + n + " сместиться с " + start + " на " + finish);
+        print(numberCircles, startRod, finishRod);
 
-        hanoi(n-1, tmp, finish);
+        hanoi(numberCircles - 1, tmpRod, finishRod);
     
     }
     
 }
 
+function print(numberCircles, startRod, finishRod) {
+
+    console.log("Кружок " + numberCircles + " сместиться с " + startRod + " на " + finishRod);
+    
+}
+
 console.log(hanoi(3, 1, 3));
+
+
 
